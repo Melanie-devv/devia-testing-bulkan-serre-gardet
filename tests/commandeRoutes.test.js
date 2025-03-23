c0e9d0f395383631df5ad481d2b4cf54d22f1860
LouisG <louisegardet@gmail.com>
2025-03-23 21:54:29 +0100

    TDD: Test - Vérifier qu'on peut récupérer une commande par ID

--- tests/commandeRoutes.test.js
+++ tests/commandeRoutes.test.js
@@ -28,3 +28,25 @@ describe('GET /commandes', () => {
         expect(res.body).to.be.an('array');
     });
 });
+
+describe('GET /commandes/:id', () => {
+    it('doit récupérer une commande par ID', async () => {
+        // On crée d'abord une commande pour avoir un ID à tester
+        const commande = await request(app)
+            .post('/commandes')
+            .send({ produit: 'Stylo', quantite: 10, prix: 1 });
+
+        const res = await request(app).get(`/commandes/${commande.body.id}`);
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.have.property('id', commande.body.id);
+        expect(res.body.produit).to.equal('Stylo');
+    });
+
+    it("renvoie une erreur 404 si l'ID n'existe pas", async () => {
+        const res = await request(app).get('/commandes/999');
+
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.have.property('message');
+    });
+});
