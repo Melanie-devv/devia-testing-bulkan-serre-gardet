10afe9c27902c4a7658da85f619f96309a8c4910
Melanie-devv <melanie.bulkan@epsi.fr>
2025-03-23 14:30:12 +0100

    BDD: test POST /etablissements

--- tests/etablissementRoutes.test.js
+++ tests/etablissementRoutes.test.js
@@ -15,10 +15,23 @@ describe('Etablissement Routes', () => {
         await sequelize.sync({ force: true }); // Reset the database
     });
 
+    it('should create a new etablissement', async () => {
+        const res = await request.post('/etablissements').send({
+            nom: 'Établissement A',
+            adresse: '123 Rue A',
+            telephone: '0123456789'
+        });
+
+        expect(res.status).to.equal(201);
+        expect(res.body).to.have.property('id');
+        expect(res.body.nom).to.equal('Établissement A');
+    });
+
     it('should retrieve all etablissements', async () => {
         const res = await request.get('/etablissements');
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array');
-        expect(res.body.length).to.be.at.least(0); // Should have 0 etablissement
+        expect(res.body.length).to.be.at.least(1); // Should have at least one etablissement
     });
+
 });
